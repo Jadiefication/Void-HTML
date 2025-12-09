851d2122ac4a8f5b0716cbc057df6ffe07a21135
jadie <jadeczjade008@gmail.com>
2025-12-09 08:01:53 +0100

    chore: jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     kotlin("jvm") version "2.2.20"
+    `maven-publish`
 }
 
 group = "io.void"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -20,4 +21,27 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+description = "void-html"
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenKotlin") {
+            artifact(tasks["jar"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+            groupId = "com.github.Jadiefication"
+            artifactId = "Void"
+            version = rootProject.version.toString()
+        }
+    }
 }
